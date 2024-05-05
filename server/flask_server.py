797e467376f021e5683e3ac48d40460feb032e9c
YZ <kokyenzein@gmail.com>
2024-05-04 22:15:39 -0400

    flask and gemini

--- server/flask_server.py
+++ server/flask_server.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
 import os
 from Gemini_AI import generate_dish, image_to_json
+import firebase_admin
+from firebase_admin import credentials, db
+import json
+
+# Initialize the Firebase Admin SDK
+cred = credentials.Certificate('firebase_service_account.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://foodframe-422304-default-rtdb.firebaseio.com'
+})
 
 app = Flask(__name__)
 
@@ -10,9 +19,16 @@ def upload_image_info():
     print(data)
     image_url = data['image_url']
     ingredient_name = data['ingredient_name']
-    # Process the data or store it
-    print([image_url, ingredient_name])
+
+    # call image_to_json
+    json_ingredient = image_to_json(ingredient_name, image_url)
+
+    # Upload json_ingredient to Firebase under the /ingredients/ path
+    ref = db.reference('/ingredients/')
+    ref.set(json_ingredient)
+
+    
     return jsonify({'status': 'success', 'message': 'Data received'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.37.35', port=int(os.environ.get('PORT', 3000)))
+    app.run(debug=True, host='localhost', port=int(os.environ.get('PORT', 3000)))
